702e1ec49e1f89689387534b0f39ba1d857a8bf4
ismael <spacingismael@gmail.com>
2023-11-14 22:43:38 +0100

    fixing rot13

--- print_format.c
+++ print_format.c
@@ -35,8 +35,8 @@ int print_format(char spec, va_list args)
 		r += print_rev(va_arg(args, char *));
 	else if (spec == 'p')
 		r += print_digit(va_arg(args, unsigned long), 16);
-	else if
-		(spec == 'R')
+	else if (spec == 'R')
+		r += print_rot13(args);
 	else
 		r += write(1, &spec, 1);
 
